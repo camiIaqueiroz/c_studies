be2ee777a3bcf6621ca2c60d1260ba0d5fa233db
camila queiroz <78580890+camiIaqueiroz@users.noreply.github.com>
2022-05-23 20:55:07 -0300

    Create segundoprograma.c

---
+++ segundoprograma.c
@@ -0,0 +1,11 @@
+#includ <stdio.h>
+
+void main()
+  
+{
+  
+  printf("Tamanho de armazenamento para o tipo int: %d \n", sizeof(int))
+  printf("Tamanho de armazenamento para o tipo short: %d \n", sizeof(short))  
+  printf("Tamanho de armazenamento para o tipo long: %d \n", sizeof(long))
+    
+  }    
