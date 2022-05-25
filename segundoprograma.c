3523a191d1dbb3f6da758125a1e624f9312cb7d5
camila queiroz <78580890+camiIaqueiroz@users.noreply.github.com>
2022-05-25 19:14:00 -0300

    Update segundoprograma.c

--- segundoprograma.c
+++ segundoprograma.c
@@ -9,3 +9,24 @@ void main()
   printf("Tamanho de armazenamento para o tipo long: %d \n", sizeof(long))
     
   }    
+
+------------------------------------------------------------------------------------
+  
+  #include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+
+{
+int idade;
+float salario, valor;
+idade = 25;
+
+
+valor = 4500.50;
+salario = valor;
+printf("Idade; %d\n", idade);
+printf("E o salario: %f\n", salario);
+
+
+}
